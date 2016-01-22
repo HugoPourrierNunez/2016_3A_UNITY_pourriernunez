532dadfc8323797a91cf256e0096ddbd813e812e
HugoPourrierNunez <pourrier789@gmail.com>
2016-01-22 18:57:56 +0100

    Dossier blender

---
+++ Unity/Assets/Script/DeplacementPerso.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            //avant
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            //arriere
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            //droite
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //gauche
+        }
+    }
+}
